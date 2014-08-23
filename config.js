bb03a80c4f633134c4d70adb4ac7994cbd434e55
Jonathan Commins <joncom@gmail.com>
2014-08-23 02:04:46 -0700

    Simplify config.

--- config.js
+++ config.js
@@ -1,23 +1,4 @@
 {
     "host": "localhost",
-    "port": 3000,
-    "rom_path": "media/ruby.gba",
-    "api": {
-        "url": "https://dev/rse/api/"
-    },
-    "servers": {
-        "login": {
-            "host": "dev2",
-            "port": 3001
-        },
-        "gateway": {
-            "host": "dev2",
-            "port": 3002,
-            "max_users": 10
-        },
-        "zone": {
-            "host": "dev2",
-            "port": 3003
-        }
-    }
+    "port": 3000
 }
